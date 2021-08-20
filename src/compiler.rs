539d29d690d20747e7ae207d1d12fbecb6dbccdc
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2021-08-21 00:11:37 +0400

    converted from oop model to functional model

---
+++ src/compiler.rs
@@ -0,0 +1,108 @@
+use std::io::prelude::*;
+use std::io;
+
+type Memtype = u8;
+const MEMSIZE: usize = 30_000;
+const ULIMIT:Memtype = 255;
+const LLIMIT:Memtype = 0;
+
+pub enum CompileError {
+    ReadError,
+    ItsTooBig,
+    ItsTooSmall
+}
+
+pub fn evaluate(code: &String) -> Result<Vec<Memtype>, CompileError> {
+    let mut memory: [Memtype; MEMSIZE] = [0; MEMSIZE];
+    let mut address:usize = 0;
+    let mut loopvar:usize = 0;
+
+    let mut output: Vec<Memtype> = Vec::new();
+    let loop_points = code
+                        .bytes()
+                        .enumerate()
+                        .filter(|&(_, x)| x == b'[')
+                        .map(|(i,_)|i)
+                        .collect::<Vec<usize>>();
+    let byte: Vec<Memtype> = code.bytes().collect();
+    let mut code_index = 0usize;
+    loop {
+        if (code_index) >= byte.len() {
+            break;
+        }
+        match byte.get(code_index) {
+            Some(&char) => {
+                match char {
+                    b'+' => { 
+                        // increment value at a memory location 
+                        if memory[address] == ULIMIT {
+                            return Err(CompileError::ItsTooBig)
+                        } else {
+                            memory[address] += 1;
+                            code_index += 1;
+                        }
+                    }
+                    b'-' => { 
+                        // decrement value at the memory location
+                        if memory[address] == LLIMIT {
+                            return Err(CompileError::ItsTooSmall)
+                        } else {
+                            memory[address] -= 1;
+                            code_index += 1;
+                        }
+                    }
+                    b'>' => { 
+                        // move right
+                        address += 1;
+                        address = address % MEMSIZE;
+                        code_index += 1;
+                    }
+                    b'<' => {
+                        // move left
+                        if address == 0 {
+                            address += MEMSIZE;
+                        } 
+                        address -= 1;
+                        code_index += 1;
+                    }
+                    b'[' => {
+                        // start the loop
+                        loopvar += 1;
+                        code_index += 1;
+                    }
+                    b']' => {
+                        // end of loop, go to its corresponding opening bracket
+                        loopvar -= 1;
+                        if memory[address] == LLIMIT {
+                            code_index += 1;
+                        } else {
+                            code_index = match loop_points.get(loopvar) {
+                                Some(&i) => i,
+                                None => panic!("")
+                            };
+                        }
+                    }
+                    b',' => {
+                        // getchar
+                        let mut buffer = [0u8];
+                        match io::stdin().read(&mut buffer) {
+                            Ok(_) => (),
+                            Err(_) => return Err(CompileError::ReadError)
+                        };
+                        memory[address] = buffer[0];
+                        code_index += 1;
+                    }
+                    b'.' => {
+                        // putchar
+                        output.push(memory[address]);
+                        code_index += 1;
+                    }
+
+                    _ => code_index += 1
+                }
+            }
+            None => {break;}
+        }
+    }
+    Ok(output)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,140 +1,8 @@
-use std::io::prelude::*;
 use std::io;
 use std::env;
 use std::fs;
 use std::time::{ Instant };
-
-type Memtype = u8;
-const MEMSIZE: usize = 30_000;
-const ULIMIT:Memtype = 255;
-const LLIMIT:Memtype = 0;
-
-enum CompileError {
-    ReadError,
-    ItsTooBig,
-    ItsTooSmall
-}
-
-struct Evaluator {
-    memory: [Memtype; MEMSIZE],
-    index: usize,
-    loopvar: usize
-}
-
-
-impl Evaluator {
-    fn new() -> Evaluator {
-        Evaluator {
-            memory: [0; MEMSIZE],
-            index: 0usize,
-            loopvar: 0usize,
-        }
-    }
-    fn evaluate(&mut self, code: &String) -> Result<Vec<Memtype>, CompileError> {
-        let mut output: Vec<Memtype> = Vec::new();
-        let loop_points = code
-                            .bytes()
-                            .enumerate()
-                            .filter(|&(_, x)| x == b'[')
-                            .map(|(i,_)|i)
-                            .collect::<Vec<usize>>();
-        let byte: Vec<Memtype> = code.bytes().collect();
-        let mut index = 0usize;
-        loop {
-            if (index) >= byte.len() {
-                break;
-            }
-            match byte.get(index) {
-                Some(&char) => {
-                    match char {
-                        b'+' => { 
-                            // increment value at a memory location 
-                            if self.memory[self.index] == ULIMIT {
-                                return Err(CompileError::ItsTooBig)
-                            } else {
-                                self.memory[self.index] += 1;
-                                index+= 1;
-                            }
-                        }
-                        b'-' => { 
-                            // decrement value at the memory location
-                            if self.memory[self.index] == LLIMIT {
-                                return Err(CompileError::ItsTooSmall)
-                            } else {
-                                self.memory[self.index] -= 1;
-                                index+= 1;
-                            }
-                        }
-                        b'>' => { 
-                            // move right
-                            self.index += 1;
-                            self.index = self.index % MEMSIZE;
-                            index+= 1;
-                        }
-                        b'<' => {
-                            // move left
-                            if self.index == 0 {
-                                self.index += MEMSIZE;
-                            } 
-                            self.index -= 1;
-                            index+= 1;
-                        }
-                        b'[' => {
-                            // start the loop
-                            self.loopvar += 1;
-                            index+= 1;
-                        }
-                        b']' => {
-                            // end of loop, go to its corresponding opening bracket
-                            self.loopvar -= 1;
-                            if self.memory[self.index] == LLIMIT {
-                                index += 1;
-                            } else {
-                                index = match loop_points.get(self.loopvar) {
-                                    Some(&i) => i,
-                                    None => panic!("")
-                                };
-                            }
-                        }
-                        b',' => {
-                            // getchar
-                            let mut buffer = [0u8];
-                            match io::stdin().read(&mut buffer) {
-                                Ok(_) => (),
-                                Err(_) => return Err(CompileError::ReadError)
-                            };
-                            self.memory[self.index] = buffer[0];
-                            index+= 1;
-                        }
-                        b'.' => {
-                            // putchar
-                            output.push(self.memory[self.index]);
-                            index+= 1;
-                        }
-
-                        _ => index += 1
-                    }
-                }
-                None => {break;}
-            }
-        }
-        Ok(output)
-    }
-    
-}
-fn process(code: &String) -> Result<(), CompileError> {
-    let mut evaluator = Evaluator::new();
-    match evaluator.evaluate(code) {
-        Ok(value) => {
-            for ascii in value {
-                print!("{}", ascii as char);
-            };
-            println!("");
-            Ok(())
-        },
-        Err(e) => Err(e)
-    }
-}
+mod compiler;
 
 fn main() {
     let args: Vec<String> = env::args()
@@ -156,12 +24,16 @@ fn main() {
                 };
 
                 // process
-                match process(&input) {
-                    Ok(_) => (), 
+                match compiler::evaluate(&input) {
+                    Ok(chars) => {
+                        for ch in chars {
+                            print!("{}", ch as char);
+                        }
+                    }, 
                     Err(e) => {
                         match e {
-                            CompileError::ItsTooSmall => println!("Dude in {}, one memory cell just became negative", file),
-                            CompileError::ItsTooBig => println!("Dude in {}, one memory cell just became soo big", file),
+                            compiler::CompileError::ItsTooSmall => println!("Dude in {}, one memory cell just became negative", file),
+                            compiler::CompileError::ItsTooBig => println!("Dude in {}, one memory cell just became soo big", file),
                             _ => println!("Dude i cant process {} file idk why :(", file)
                         }
                         continue;
@@ -182,12 +54,16 @@ fn main() {
         };
 
         // process
-        match process(&input) {
-            Ok(_) => (), 
+        match compiler::evaluate(&input) {
+            Ok(chars) => {
+                for ch in chars {
+                    print!("{}", ch as char);
+                }
+            }, 
             Err(e) => {
                 match e {
-                    CompileError::ItsTooSmall => println!("Dude one memory cell just became negative"),
-                    CompileError::ItsTooBig => println!("Dude one memory cell just became soo big"),
+                    compiler::CompileError::ItsTooSmall => println!("Dude one memory cell just became negative"),
+                    compiler::CompileError::ItsTooBig => println!("Dude one memory cell just became soo big"),
                     _ => println!("Dude i cant process this idk why :(")
                 }
             }
@@ -199,9 +75,8 @@ fn main() {
 
 #[test]
 fn parse() {
-    let mut evaluator = Evaluator::new();
     let code = "++++++++++[>++++++++++<-]>.";
-    assert_eq!(evaluator.evaluate(&code.to_string()), Vec::from([100]));
+    assert_eq!(compiler::evaluate(&code.to_string()), Vec::from([100]));
 }
 
 #[test]
