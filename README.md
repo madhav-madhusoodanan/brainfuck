44c38d297ab95da4993656f1ecfc9e255abbaba7
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2021-08-20 15:47:28 +0400

    completed

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# brainfuck (incomplete)
+# brainfuck (complete)
+
 A compiler written in rust, for code written in brainfuck
 
 Visit this [site](https://gist.github.com/roachhd/dce54bec8ba55fb17d3a) to learn the language.<br />
-
-
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use std::io;
 use std::env;
 use std::fs;
 
+enum CompileError {
+    ParseError
+}
+
 struct Evaluator {
     memory: [u8; 30_000],
     index: usize,
@@ -18,36 +22,49 @@ impl Evaluator {
             loopvar: 0usize,
         }
     }
-    fn evaluate(&mut self, code: &str) {
-        let mut loopPoints: Vec<usize> = Vec::new();
+    fn evaluate(&mut self, code: &String) -> Result<Vec<u8>, CompileError> {
+        let mut output: Vec<u8> = Vec::new();
+        let loop_points = code
+                            .bytes()
+                            .enumerate()
+                            .filter(|&(_, x)| x == b'[')
+                            .map(|(i,_)|i)
+                            .collect::<Vec<usize>>();
         let byte: Vec<u8> = code.bytes().collect();
         let mut index = 0usize;
         loop {
             self.index = self.index % 30_000;
-            index = index % byte.len();
+            if (index) >= byte.len() {
+                break;
+            }
             match byte.get(index) {
                 Some(&char) => {
                     match char {
                         b'+' => { 
                             // increment value at a memory location 
                             self.memory[self.index] += 1;
+                            index+= 1;
                         }
                         b'-' => { 
                             // decrement value at the memory location
                             self.memory[self.index] -= 1;
+                            index+= 1;
+
                         }
                         b'>' => { 
                             // move right
                             self.index += 1;
+                            index+= 1;
                         }
                         b'<' => { 
                             // move left
-                            self.index -= 1
+                            self.index -= 1;
+                            index+= 1;
                         }
                         b'[' => {
                             // start the loop
-                            loopPoints.push(self.loopvar);
                             self.loopvar += 1;
+                            index+= 1;
                         }
                         b']' => {
                             // end of loop, go to its corresponding opening bracket
@@ -55,10 +72,10 @@ impl Evaluator {
                             if self.memory[self.index] == 0 {
                                 index += 1;
                             } else {
-                                index = match loopPoints.get(self.loopvar) {
+                                index = match loop_points.get(self.loopvar) {
                                     Some(&i) => i,
                                     None => panic!("")
-                                }
+                                };
                             }
                         }
                         b',' => {
@@ -66,33 +83,73 @@ impl Evaluator {
                             let mut buffer = [0u8];
                             io::stdin().read(&mut buffer);
                             self.memory[self.index] = buffer[0];
+                            index+= 1;
                         }
                         b'.' => {
                             // putchar
-                            print!("{}", self.memory[self.index] as char);
+                            output.push(self.memory[self.index]);
+                            index+= 1;
                         }
 
                         _ => ()
                     }
                 }
-                None => ()
+                None => {break;}
             }
         }
+        Ok(output)
     }
     
 }
-
-
-fn main() {
-    
+fn process(code: &String) -> Result<(), CompileError> {
+    let mut evaluator = Evaluator::new();
+    match evaluator.evaluate(code) {
+        Ok(value) => {
+            for ascii in value {
+                print!("{}", ascii as char);
+            };
+            Ok(())
+        },
+        Err(e) => Err(e)
+    }
 }
 
-// #[test]
-// fn parse() {
-//     let mut evaluator = Evaluator::new();
-//     let code = "++++.";
-//     evaluator.evaluate(code);
-// }
+fn main() {
+    let args: Vec<String> = env::args()
+                                .enumerate()
+                                .filter(|&(index, _)| index != 0)
+                                .map(|(_, elem)| elem)
+                                .collect();
+    if args.len() != 0 {
+        for file in args{
+                let input = fs::read_to_string(&file).expect("ouch!");
+                match process(&input) {
+                    Ok(_) => (), 
+                    Err(_) => panic!("Error")
+                }
+
+                
+        }
+    } else if args.len() == 0 {
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => (),
+            Err(_) => panic!("Error")
+        };
+        match process(&input) {
+            Ok(_) => (), 
+            Err(_) => panic!("Error")
+        }
+    }
+
+}
+
+#[test]
+fn parse() {
+    let mut evaluator = Evaluator::new();
+    let code = "++++++++++[>++++++++++<-]>.";
+    assert_eq!(evaluator.evaluate(&code.to_string()), Vec::from([100]));
+}
 
 #[test]
 fn string () {
